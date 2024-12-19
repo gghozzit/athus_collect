133388fe4bef331fab6bf08f2ff04e402d0f00c8
Ahmad Ghozali <78840806+gghozzit@users.noreply.github.com>
2024-12-19 09:16:45 +0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Input Data Athus",
   description: "Input Data Athus Sementara oleh Gojit",
+  icons: {
+    icon: "public/BRIN-Logo.svg",
+  },
 };
 
 export default function RootLayout({
